c2d771c1cd8b01a0577601a1fcd06a689203b604
Ralph Moran <ralphmoran2003@gmail.com>
2018-04-19 01:08:51 -0700

    Modified gulpfile to compile SASS

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # gulp_sass
+Compiling SASS to CSS, then minify thems
---
+++ gulpfile.js
@@ -0,0 +1,13 @@
+var gulp      = require('gulp'     );
+var sass      = require('gulp-sass');
+var minifycss = require('gulp-csso');
+
+gulp.task('compile-sass', function(){
+    return gulp.src('app/scss/*.scss')
+            .pipe(sass())
+            .pipe(minifycss
+            ())
+            .pipe(gulp.dest('dist/css'));
+});
+
+gulp.task('default', ['compile-sass']);
